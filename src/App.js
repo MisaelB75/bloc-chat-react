ffe3d293975f734a8461f4df8b29fc86db060d45
Misael B <misaelb@gmail.com>
2018-07-24 21:17:49 +0900

    Checkpoint BlocChat ListRooms: Completed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+
+//<script src="https://www.gstatic.com/firebasejs/5.3.0/firebase.js"></script>
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDZwUwJ7F-hwB0slFLeznro2i5l-7gjDIU",
+    authDomain: "blocchat-react-210ba.firebaseapp.com",
+    databaseURL: "https://blocchat-react-210ba.firebaseio.com",
+    projectId: "blocchat-react-210ba",
+    storageBucket: "blocchat-react-210ba.appspot.com",
+    messagingSenderId: "30938452332"
+  };
+  firebase.initializeApp(config);
+
+  class App extends Component {
+    render() {
+
+      return (
+        <div className="App">
+          <header>
+             <h1>Bloc Chat</h1>
+          </header>
+          <RoomList firebase={firebase} />
+          <main>
+
+          </main>
+        </div>
+      );
+    }
   }
-}
 
 export default App;
